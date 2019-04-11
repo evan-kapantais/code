b002eeadd1b5ca2676fe3c6cfee5c81015a4ab8f
evan-kapantais <evankapantais@gmail.com>
2019-04-11 21:53:51 +0200

    code finalized

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 (document.onload = () => {
+  console.clear();
   
     const inputs = document.getElementsByTagName("input");
     const submit = document.querySelector(".btn");
+    const card = document.querySelector(".container");
+
+    let theCode = [];
+  
+    card.addEventListener("click", handleClick);
 
   for (let i = 0; i < inputs.length; i++) {
     inputs[i].addEventListener("keyup", () => handleInput(i));
@@ -11,18 +17,45 @@
 
     if(inputs[index].value.length == 1) {
 
-      if (index  == inputs.length - 1) {
+      if (index < inputs.length - 1) {
+
+        theCode.push(inputs[index].value);
+        inputs[index].disabled = true;
+        inputs[index + 1].disabled = false;
+        inputs[index + 1].focus();
+
+      } else {
+
+        theCode.push(inputs[index].value);
+        inputs[index].disabled = true;
+        submit.addEventListener("click", function(){
+          alert(theCode.join(" - "));
+          });
         submit.focus();
+      }      
+      
+    } else {
+      inputs[index].value = inputs[index].value.slice(0, 1);
+    }
+
+    if (event.keyCode == "8") {
+
+      if (index == 0) {
         return;
       }
 
       inputs[index].disabled = true;
-      inputs[index + 1].focus();
-    }
-
-    if (event.keyCode == "8") {
-      console.log("backspace");
+      inputs[index - 1].value = "";
+      inputs[index - 1].disabled = false;
+      inputs[index - 1].focus();
     }
   }
-
+  
+  function handleClick() {
+    for (let i = 0; i < inputs.length; i++) {
+      if (inputs[i].value == "") {
+        inputs[i].focus();
+      }
+    }
+  } 
 })();
